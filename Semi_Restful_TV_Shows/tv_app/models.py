92164362ef3a5fe2182676f6de1f929cea8ddcfe
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-09 12:49:16 -0800

    Added a validation that checks to make sure a title doesn't already exist

--- Semi_Restful_TV_Shows/tv_app/models.py
+++ Semi_Restful_TV_Shows/tv_app/models.py
@@ -10,21 +10,23 @@ class ShowManager(models.Manager):
         today = datetime.now()
         user = datetime.strptime(postData['date'], "%Y-%m-%d")
         print(today, user)
+        if Show.objects.filter(title=postData['title']):
+            errors['unique'] = "Show tile already exist!"
         if len(postData['title']) < 2:
-            errors['title'] = "Show tile should be at least 2 characters"
+            errors['title'] = "Show tile should be at least 2 characters!"
             print('title error')
         if len(postData['network']) < 3:
-            errors['network'] = "Show network should be at least 3 characters"
+            errors['network'] = "Show network should be at least 3 characters!"
             print('network error')
         if user.date() >= today.date():
             print('date error')
             if user.date() == today.date():
-                errors['date'] = "The show's release date can't be Today"
+                errors['date'] = "The show's release date can't be Today!"
             elif user.date() > today.date():
-                errors['date'] = "The show's release date can't be the Future"
+                errors['date'] = "The show's release date can't be the Future!"
         if postData['desc']:
             if len(postData['desc']) < 10:
-                errors['desc'] = "Show description should be at least 10 characters"
+                errors['desc'] = "Show description should be at least 10 characters!"
                 print('description error')
         return errors
 
